d9c8a906bc0518f19188c3c1c041956e678389b2
liniyakamnya <liniyakamnya@gmail.com>
2017-10-28 21:46:14 +0300

    Logging, github oauth

--- oauth2/resource/src/main/java/demo/ResourceApplication.java
+++ oauth2/resource/src/main/java/demo/ResourceApplication.java
@@ -4,7 +4,6 @@ import org.apache.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
