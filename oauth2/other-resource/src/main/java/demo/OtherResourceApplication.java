39dd45c974767bf4c77812750e52fed9dcde3e98
liniyakamnya <liniyakamnya@gmail.com>
2017-11-20 20:55:32 +0300

    Resource Id for resources

--- oauth2/other-resource/src/main/java/demo/OtherResourceApplication.java
+++ oauth2/other-resource/src/main/java/demo/OtherResourceApplication.java
@@ -1,25 +1,30 @@
 package demo;
 
-import java.util.UUID;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.RemoteTokenServices;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.UUID;
+
 @SpringBootApplication
 @RestController
+@Configuration
+
 @EnableResourceServer
-public class OtherResourceApplication {
+public class OtherResourceApplication extends ResourceServerConfigurerAdapter {
 
     private static Logger LOG = Logger.getLogger(OtherResourceApplication.class);
 
@@ -47,7 +52,7 @@ public class OtherResourceApplication {
 
     @RequestMapping("/")
     public Message home(HttpServletRequest request,
-            HttpServletResponse response) {
+                        HttpServletResponse response) {
 
         LOG.info("Authorization: " + request.getHeader("Authorization"));
         return new Message("Hello World from other resource");
@@ -57,6 +62,21 @@ public class OtherResourceApplication {
     public Message custom() {
 
         return new Message("Hello World");
+    }
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        // @formatter:off
+        http
+                .requestMatchers().antMatchers("/**")
+                .and()
+                .authorizeRequests().anyRequest().access("#oauth2.hasScope('resource-read')");
+        // @formatter:on
+    }
+
+    @Override
+    public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+        resources.resourceId("resource-id2");
     }
 
     class Message {
--- oauth2/resource/src/main/java/demo/ResourceApplication.java
+++ oauth2/resource/src/main/java/demo/ResourceApplication.java
@@ -1,25 +1,29 @@
 package demo;
 
-import java.util.UUID;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.RemoteTokenServices;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.UUID;
+
 @SpringBootApplication
 @RestController
+@Configuration
 @EnableResourceServer
-public class ResourceApplication {
+public class ResourceApplication extends ResourceServerConfigurerAdapter {
 
     private static Logger LOG = Logger.getLogger(ResourceApplication.class);
 
@@ -46,8 +50,7 @@ public class ResourceApplication {
     }
 
     @RequestMapping("/")
-    public Message home(HttpServletRequest request,
-            HttpServletResponse response) {
+    public Message home(HttpServletRequest request, HttpServletResponse response) {
 
         LOG.info("Authorization: " + request.getHeader("Authorization"));
         return new Message("Hello World");
@@ -57,6 +60,21 @@ public class ResourceApplication {
     public Message custom() {
 
         return new Message("Hello World");
+    }
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        // @formatter:off
+        http
+                .requestMatchers().antMatchers("/**")
+                .and()
+                .authorizeRequests().anyRequest().access("#oauth2.hasScope('resource-read')");
+        // @formatter:on
+    }
+
+    @Override
+    public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+        resources.resourceId("resource-id1");
     }
 
     class Message {
